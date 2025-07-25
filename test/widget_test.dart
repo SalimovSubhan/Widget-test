dd806f13d518e1a00cafde96fff5986f763d7576
SalimovSubhan <subhansalimov08@gmail.com>
2025-07-25 10:58:13 +0300

    counter test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,23 +8,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:widget_test/main.dart';
+import 'package:widget_test/widgets/counter_widget.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
+  testWidgets('Кнопка увеличивает счетчик', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: Scaffold(body: CounterWidget())));
     expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byKey(Key('increment')));
     await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
 }
